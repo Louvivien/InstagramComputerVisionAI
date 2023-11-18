04253bbcc09a3dd4480feed281a3bd8c2e53f27e
Vivien Richaud <vivien@MacBook-Pro-de-Vivien.local>
2023-11-18 15:56:13 +0100

    max token 500

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ def generate_description(frames):
         chat_completion = client.chat.completions.create(
             messages=prompt_messages,
             model="gpt-4-vision-preview",
-            max_tokens=1000  
+            max_tokens=500  
 
         )
         return chat_completion.choices[0].message.content
