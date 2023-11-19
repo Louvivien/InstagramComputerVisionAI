117394d233ec26d08f788aa9816c2d0806d88e2c
Vivien Richaud <vivien@MacBook-Pro-de-Vivien.local>
2023-11-19 11:39:09 +0100

    frontend

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import os
 import cv2
 import base64
@@ -5,16 +6,8 @@ from openai import OpenAI
 from instagrapi import Client
 from dotenv import load_dotenv
 
-# Load environment variables
-root_dir = os.path.dirname(os.path.abspath(__file__))
-dotenv_path = os.path.join(root_dir, '.env')
-load_dotenv(dotenv_path)
-
-username = os.getenv('INSTA_USERNAME')
-password = os.getenv('INSTA_PASSWORD')
-openai_api_key = os.getenv('OPENAI_API_KEY')
-
-def download_videos(target_profile):
+# Function to download videos from Instagram
+def download_videos(target_profile, username, password):
     cl = Client()
     cl.login(username, password)
 
@@ -28,31 +21,26 @@ def download_videos(target_profile):
     for media in medias:
         if media.media_type == 2:  # 2 represents a video
             video_path = cl.video_download(media.pk, folder='./videos')
-            print(f"Downloaded video path: {video_path}")  # Debugging line
+            st.write(f"Downloaded video path: {video_path}")
             video_paths.append(video_path)
-
 
     return video_paths
 
-
-
-
+# Function to extract frames from a video
 def extract_frames(video_path, max_frames=5):
     if not os.path.exists(video_path):
-        print(f"Video file not found: {video_path}")
+        st.write(f"Video file not found: {video_path}")
         return []
 
-    print(f"Opening video file: {video_path}")
+    st.write(f"Opening video file: {video_path}")
     absolute_video_path = os.path.abspath(video_path)
     video = cv2.VideoCapture(absolute_video_path)
 
-    # Calculate the total number of frames and the interval for frame extraction
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     frame_interval = total_frames // max_frames
 
     base64_frames = []
     for i in range(max_frames):
-        # Set the video position to the frame number
         frame_number = i * frame_interval
         video.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
 
@@ -66,12 +54,8 @@ def extract_frames(video_path, max_frames=5):
     video.release()
     return base64_frames
 
-
-
-
-
-
-def generate_description(frames):
+# Function to generate description using OpenAI
+def generate_description(frames, openai_api_key):
     client = OpenAI(api_key=openai_api_key)
     prompt_messages = [
         {
@@ -87,42 +71,56 @@ def generate_description(frames):
         chat_completion = client.chat.completions.create(
             messages=prompt_messages,
             model="gpt-4-vision-preview",
-            max_tokens=500  
-
+            max_tokens=500
         )
         return chat_completion.choices[0].message.content
     except Exception as e:
-        print(f"An error occurred: {e}")
+        st.write(f"An error occurred: {e}")
         return None
-    
-    
-    
-def generate_voice(description, filename):
-    client = OpenAI(api_key=openai_api_key)  # Ensure you use the API key here
+
+# Function to generate voice narration
+def generate_voice(description, filename, openai_api_key):
+    client = OpenAI(api_key=openai_api_key)
     response = client.audio.speech.create(
         model="tts-1",
         voice="alloy",
         input=description,
     )
 
-    # Check if mp3 directory exists, if not create it
     if not os.path.exists('mp3'):
         os.makedirs('mp3')
 
     mp3_path = os.path.join('mp3', f"{filename}.mp3")
     response.stream_to_file(mp3_path)
-    print(f"Narration saved for file: {filename}")
+    st.write(f"Narration saved for file: {filename}")
+    return mp3_path
 
+# Streamlit app
+def main():
+    st.title("Instagram Video Description Generator")
+
+    # Environment variables input
+    st.sidebar.header("Environment Variables")
+    insta_username = st.sidebar.text_input("Instagram Username")
+    insta_password = st.sidebar.text_input("Instagram Password", type="password")
+    openai_api_key = st.sidebar.text_input("OpenAI API Key")
+
+    # Target profile input
+    target_profile = st.text_input("Enter the Instagram profile to analyze:", value="apple")
+
+    if st.button("Analyze Videos"):
+        video_files = download_videos(target_profile, insta_username, insta_password)
+        for video_file in video_files:
+            frames = extract_frames(video_file, max_frames=5)
+            description = generate_description(frames, openai_api_key)
+            st.write(description)
+
+            video_filename = os.path.splitext(os.path.basename(video_file))[0]
+            mp3_path = generate_voice(description, video_filename, openai_api_key)
+
+            # Display MP3 file
+            if os.path.exists(mp3_path):
+                st.audio(mp3_path)
 
 if __name__ == "__main__":
-    target_profile = 'apple'
-    video_files = download_videos(target_profile)
-
-    for video_file in video_files:
-        frames = extract_frames(video_file, max_frames=5)  # Adjust max_frames as needed
-        description = generate_description(frames)
-        print(description)
-
-        # Extract filename without extension and pass it to generate_voice
-        video_filename = os.path.splitext(os.path.basename(video_file))[0]
-        generate_voice(description, video_filename)
+    main()
